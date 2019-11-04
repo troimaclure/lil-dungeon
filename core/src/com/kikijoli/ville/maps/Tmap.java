ee7e8539a40b013274a15ee63840251c736a1050
Arthur JOSSEAU <arthur.josseau@milleis.fr>
2019-11-04 14:05:16 +0100

    GL_NORAMLIZE

--- core/src/com/kikijoli/ville/maps/Tmap.java
+++ core/src/com/kikijoli/ville/maps/Tmap.java
@@ -183,7 +183,7 @@ public class Tmap implements Screen {
         ShaderManager.step();
         Gdx.gl.glEnable(0x0BA1);
         Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         worldCoordinates = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
         CameraManager.tour();
 
